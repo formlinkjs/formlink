c84b47ad0fef9f7854a4e2b6ca41b9084b8b31f3
Jerome (Thavarshan) Thayananthajothy <jerome@netwatchglobal.com>
2023-10-07 02:35:18 +0100

    Add production build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,34 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import typescript from "@rollup/plugin-typescript";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
+        manifest: true,
+        minify: true,
+        reportCompressedSize: true,
         lib: {
             entry: path.resolve(__dirname, './src/index.ts'),
             name: 'formlink',
+            formats: ['es', 'cjs'],
             fileName: format => `formlink.${format}.js`
-        }
+        },
+        rollupOptions: {
+            external: [],
+            plugins: [
+                typescriptPaths({
+                    preserveExtensions: true,
+                }),
+                typescript({
+                    sourceMap: false,
+                    declaration: true,
+                    outDir: "dist",
+                }),
+            ],
+        },
     },
 
     resolve: {
